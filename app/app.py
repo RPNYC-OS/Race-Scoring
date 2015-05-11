89ab90b043478b5f9a24d6a0e72e74ea679f5747
Campbell Fleury <campbell_fleury@live.com>
2015-05-11 22:38:51 +1200

    Initial code

--- app/app.py
+++ app/app.py
@@ -1,3 +1,10 @@
 # Title: Race Score Module
 # Author: Campbell Fleury
 # Date: 2015-05-11
+
+class boat(object):
+
+
+    def __init__(self,name,sailno):
+        self.name = name
+        self.sailno = sailno
